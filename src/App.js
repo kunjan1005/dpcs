ed7f606b3054555cdc4b5e165473aa24f49e0b5a
Saurabh <saurabh.bhatt.saubhagyam@gmail.com>
2021-11-15 12:52:30 +0530

    some errors

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Header from './common/Header';
 // import  Post  from './common/Post';
 import { Route, Routes } from 'react-router';
 import Home from './components/Home';
+import Profile from './components/Profile'
+import Explore from './components/Explore'
+import Notifications from './components/Notification'
 
 function App() {
   return (
@@ -15,6 +18,11 @@ function App() {
        <div className='container-fluid '>
        <Routes>
          <Route exact path='/' element={<Home/>}></Route>
+         <Route exact path='/login' element={<Login/>}></Route>
+         <Route exact path='/explore' element={<Explore/>}></Route>
+         <Route exact path='/notifications' element={<Notifications/>}></Route>
+         <Route exact path='/profile' element={<Profile/>}></Route>
+
        </Routes>
             
        </div>
--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -3,6 +3,7 @@ import HomeIcone from '@material-ui/icons/Home';
 import ExploreOutlined from '@material-ui/icons/ExploreOutlined'
 import PlusOneRounded from '@material-ui/icons/PlusOneRounded'
 import NotificationsActiveIcon  from '@material-ui/icons/NotificationsActive'
+import Tooltip from '@material-ui/core/Tooltip';  
 import Profile from '@material-ui/icons/PersonOutline'
 import {NavLink} from 'react-router-dom'
 
@@ -15,9 +16,9 @@ const Header=()=>{
             <div className="container">
                 <div className="row">
                
-                    <div className="col-lg-2 col-sm-2 col-3 order-1">
+                    <div className="col-lg-2 col-sm-2 col-3 order-1 logo-div">
                         <div className="logo_container">
-                            <div className="logo"><a href="#">BBB</a></div>
+                            <div className="logo"><img src="./../public/imges/img_logo.png" alt="logo" /></div>
                         </div>
                     </div> 
                     <div className="col-lg-6 col-12 order-lg-2 order-3 text-lg-left text-right">
@@ -31,27 +32,32 @@ const Header=()=>{
                             </div>
                         </div>
                     </div>
-                    <div className="col-lg-4 col-9 order-lg-3 order-2 text-lg-left">
+                    <div className="col-lg-4 col-4 order-lg-3 order-1 ">
                         <div className="wishlist_cart d-flex flex-row align-items-center justify-content-end">
                             <div className="d-flex flex-row align-items-center justify-content-end m-3">
-                               <NavLink to='/'><div className="wishlist_icon"><HomeIcone/></div></NavLink>
-                               
+                                <Tooltip title='home'>
+                               <NavLink className='links' activeclass='active' to='/'><div className="wishlist_icon"><HomeIcone/></div></NavLink>
+                                </Tooltip>
                             </div> 
                             <div className="d-flex flex-row align-items-center justify-content-end m-3">
-                                <div className="wishlist_icon"><ExploreOutlined/></div>
-                               
+                                <Tooltip title='explore'>
+                                <NavLink className='links'  activeclass='active' to='/explore'><div className="wishlist_icon"><ExploreOutlined/></div></NavLink>
+                                 </Tooltip>
                             </div> 
                             <div className="d-flex flex-row align-items-center justify-content-end m-3">
-                                <div className="wishlist_icon"><PlusOneRounded/></div>
-                               
+                                <Tooltip title='new post'>
+                                <NavLink className='links'  activeclass='active' to='/post'><div className="wishlist_icon"><PlusOneRounded/></div></NavLink>
+                                </Tooltip>
                             </div> 
                             <div className="d-flex flex-row align-items-center justify-content-end m-3">
-                                <div className="wishlist_icon"><NotificationsActiveIcon/></div>
-                               
+                                <Tooltip title='notifications'>
+                              <NavLink className='links'  activeclass='active' to='/notifications'><div className="wishlist_icon"><NotificationsActiveIcon/></div></NavLink>
+                               </Tooltip>
                             </div> 
                             <div className="d-flex flex-row align-items-center justify-content-end m-3">
-                                <div className="wishlist_icon"><Profile/></div>
-                               
+                                <Tooltip title='profile'>
+                               <NavLink className='links'  activeclass='active' to='/profile'><div className="wishlist_icon"><Profile/></div></NavLink>
+                               </Tooltip>
                             </div> 
                            
                         </div>
