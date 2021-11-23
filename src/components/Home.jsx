66c14a03a48aecffd78bbaa4f842732341ae45d8
Saurabh <saurabh.bhatt.saubhagyam@gmail.com>
2021-11-23 11:34:15 +0530

    login auth added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect } from "react";
 import Post from "../common/Post";
+import Login from  '../components/Login';
 import PerfectScrollbar from 'react-perfect-scrollbar'
 import {useSelector,useDispatch} from 'react-redux'
 
 
 const Home=()=>{
-
+    if(~loggin){
+        return <Login/>
+    }
     return(
         <div className="container-fluid p-2">
-
               <Post/>
       
         </div>
