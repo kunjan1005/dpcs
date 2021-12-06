8797efaae54c5bc4c463f290127727c039b8ed99
Saurabh <saurabh.bhatt.saubhagyam@gmail.com>
2021-12-06 17:35:31 +0530

    authorization

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Post from "../common/Post";
 import login from '../actions'
-const Home = () => {   
+
+const Home = () => {  
     return (
         <div className="container-fluid p-2">
             <Post />
--- src/env.js
+++ src/env.js
@@ -1,5 +1,7 @@
 const env={
     URL:process.env.NODE_ENV=='production'?'https://www.dipicious.com':'https://www.dipicious.com',
     JWT_SEC_KEY:'jgsfjdfgkhgldghjgliughdjfklgshusj',
+    REACT_APP_AUTH0_DOMAIN:"dev-ailau-r1.us.auth0.com",
+    REACT_APP_AUTH0_CLIENT_ID:"jnJMHOqUUBq82to4TzJk7sO4T39XTOI5",
 }
 export default env
