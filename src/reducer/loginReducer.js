58973cc63eab9576d6010b374995a14305d28669
Saurabh <saurabh.bhatt.saubhagyam@gmail.com>
2021-12-06 17:46:22 +0530

    new development

--- src/reducer/loginReducer.js
+++ src/reducer/loginReducer.js
@@ -8,16 +8,16 @@ const login = {
 const loginReducer = async (state = login, action) => {
     switch (action.type) {
         case 'LOGIN':
-            let token=localStorage.getItem('token')
-            let isValid = jwt.verify(token, env.JWT_SEC_KEY)
-            if (isValid) {
-                let user = await jwt_decode(token)
-                if (user == undefined) {
-                    return { isLogin: true, user }
-                } else {
-                    return { isLogin: false,user:{} }
-                }
-            }
+            // let token=localStorage.getItem('token')
+            // let isValid = jwt.verify(token, env.JWT_SEC_KEY)
+            // if (isValid) {
+            //     let user = await jwt_decode(token)
+            //     if (user == undefined) {
+            //         return { isLogin: true, user }
+            //     } else {
+            //         return { isLogin: false,user:{} }
+            //     }
+            // }
         case 'LOGOUT':
             return true
         default: return login
--- src/reducer/userReducer.js
+++ src/reducer/userReducer.js
@@ -5,15 +5,15 @@ const initialState={}
   const userReducer=(state=initialState,action)=>{
       switch(action.type){       
           case 'GET_PROFILE': 
-          let token = localStorage.getItem('token')
-          console.log(token)
-          let isValid = jwt.verify(token, env.JWT_SEC_KEY)
-          if (isValid) {
-              let user = jwt_decode(token)
-              if (user != undefined) {
-                 return {...user}
-              }
-          }
+        //   let token = localStorage.getItem('token')
+        //   console.log(token)
+        //   let isValid = jwt.verify(token, env.JWT_SEC_KEY)
+        //   if (isValid) {
+        //       let user = jwt_decode(token)
+        //       if (user != undefined) {
+        //          return {...user}
+        //       }
+        //   }
            
           case 'STORE_PROFILE':
               let user=action.payload 
