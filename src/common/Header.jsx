1f833b7ee3a79b3426af8f223c3e14f0694f036d
samrat-barot <samrat.saubhagyam@gmail.com>
2021-12-21 14:28:31 +0530

    adding header icon

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -1,32 +1,36 @@
-import React,{useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from '../images/dpcs_logo.png'
-import home from '../images/home.png'
+import home from '../images/icon/home.png'
+import explore from '../images/icon/explore.png'
+import bell from '../images/icon/bell.png'
+import add from '../images/icon/add.png'
+import user from '../images/icon/user.png'
 import HomeIcone from '@material-ui/icons/HomeOutlined';
 import ExploreOutlined from '@material-ui/icons/ExploreOutlined'
 import DirectionsRunIcon from '@material-ui/icons/DirectionsRunOutlined';
-import  TouchAppIcon from '@material-ui/icons/TouchAppOutlined';
+import TouchAppIcon from '@material-ui/icons/TouchAppOutlined';
 import Tooltip from '@material-ui/core/Tooltip';
 import Profile from '@material-ui/icons/PersonOutlineOutlined'
 import { NavLink } from 'react-router-dom'
-import {getProfile} from '../actions/index'
-import { useSelector,useDispatch } from "react-redux";
+import { getProfile } from '../actions/index'
+import { useSelector, useDispatch } from "react-redux";
 import { isUserLoging } from '../authorization/useAuth';
 import * as rdd from 'react-device-detect'
 
 // import Custominput from '../custom/CustomInput'
 const Header = () => {
-     let [isLogin,setLogin]=useState(false)
-     let [curUser,setUser]=useState({})
-    useEffect(()=>{
-        let {login,user}=isUserLoging()
+    let [isLogin, setLogin] = useState(false)
+    let [curUser, setUser] = useState({})
+    useEffect(() => {
+        let { login, user } = isUserLoging()
         setLogin(login)
         setUser(user)
-      
-    },[1])
+
+    }, [1])
     // console.log(rdd)
     return (
 
-        
+
         <header className="header">
             <div className="header_main">
                 <div className="container">
@@ -38,15 +42,15 @@ const Header = () => {
                         </div>
                         <div className="offset-2 col-lg-3 col-sm-2 col-2 order-1 logo-div header_search p-1">
                             <div className="logo_container">
-                                    <div className="serach_box">
+                                <div className="serach_box">
                                     <i class="fa fa-search" aria-hidden="true"></i>
-                                    <input type="search" id="" className="search_input"/>
+                                    <input type="search" id="" className="search_input" />
 
-                                    </div>
-                                    {/* <Custominput/> */}
+                                </div>
+                                {/* <Custominput/> */}
                             </div>
                         </div>
-                
+
 
                         <div className="col-lg-3 col-12 order-lg-3 order-1 offset-lg-1">
                             <div className="wishlist_cart d-flex flex-row align-items-center justify-content-end ">
@@ -54,30 +58,46 @@ const Header = () => {
                                     <Tooltip title='home'>
                                         <NavLink className='links' activeclass='active' to='/'><div className="wishlist_icon">
                                             {/* <HomeIcone className='header_btn' /> */}
-                                            <img src={home}/>
-                                            </div></NavLink>
+                                            <img src={home} />
+                                        </div></NavLink>
                                     </Tooltip>
                                 </div>
                                 <div className="d-flex flex-row align-items-center justify-content-end  nav_link">
                                     <Tooltip title='explore'>
                                         <NavLink className='links' activeclass='active' to='/explore#cuisines'><div className="wishlist_icon">
-                                            <ExploreOutlined  className='header_btn'/>
-                                            </div></NavLink>
+                                            {/* <ExploreOutlined  className='header_btn'/> */}
+                                            <img src={explore} />
+                                        </div>
+                                        </NavLink>
                                     </Tooltip>
                                 </div>
                                 <div className="d-flex flex-row align-items-center justify-content-end  nav_link">
                                     <Tooltip title='dip'>
-                                        <NavLink className='links' activeclass='active' to='/dip#dipin'><div className="wishlist_icon"><TouchAppIcon  className='header_btn' /></div></NavLink>
+                                        <NavLink className='links' activeclass='active' to='/dip#dipin'><div className="wishlist_icon">
+                                            {/* <TouchAppIcon className='header_btn' /> */}
+                                            </div>
+                                            <img src={add}/>
+                                        </NavLink>
                                     </Tooltip>
                                 </div>
                                 <div className="d-flex flex-row align-items-center justify-content-end  nav_link">
                                     <Tooltip title='notifications'>
-                                        <NavLink className='links' activeclass='active' to='/activities'><div className="wishlist_icon"><DirectionsRunIcon  className='header_btn'/></div></NavLink>
+                                        <NavLink className='links' activeclass='active' to='/activities'>
+                                            <div className="wishlist_icon">
+                                                {/* <DirectionsRunIcon className='header_btn' /> */}
+                                                <img src={bell}/>
+                                            </div>
+                                        </NavLink>
                                     </Tooltip>
                                 </div>
                                 <div className="d-flex flex-row align-items-center justify-content-end  nav_link">
                                     <Tooltip title='profile'>
-                                        <NavLink className='links' activeclass='active' to='/profile'><div className="wishlist_icon"><Profile  className='header_btn'   /></div></NavLink>
+                                        <NavLink className='links' activeclass='active' to='/profile'>
+                                            <div className="wishlist_icon">
+                                                {/* <Profile className='header_btn' /> */}
+                                                <img src={user}/>
+                                            </div>
+                                        </NavLink>
                                     </Tooltip>
                                 </div>
 
