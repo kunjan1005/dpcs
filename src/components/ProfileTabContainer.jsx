588503acc5c8dad1d9f6c8c9fb8e62409bccee1d
Saurabh <saurabh.bhatt.saubhagyam@gmail.com>
2021-12-23 11:32:26 +0530

    profile tabs

--- src/components/ProfileTabContainer.jsx
+++ src/components/ProfileTabContainer.jsx
@@ -12,7 +12,10 @@ import { isUserLoging } from "../authorization/useAuth";
 import Activity from "../common/Activity";
 import Favriate from "../common/Favraite";
 import { useDispatch, useSelector} from "react-redux";
-import { userActivity, userFavorites,userfeedback } from "../actions";
+import { userActivity, userFavorites,userfeedback,userPoints } from "../actions";
+import Review from "../common/Review";
+import Point from "./Points";
+
 const ProfileTabContainer = () => {
     let state=useSelector((state)=>state.profileTabReducer)
     let location = useLocation()
@@ -22,6 +25,7 @@ const ProfileTabContainer = () => {
         dispatch(userActivity())
         dispatch(userFavorites())
         dispatch(userfeedback())
+        dispatch(userPoints())
     }, [0])
     return (
         <div className='col-lg-8 col-sm-12 m-auto profile_post_container'>
@@ -29,14 +33,14 @@ const ProfileTabContainer = () => {
             <div className="row m-auto">
                 <div className='col-md-3 col-3'><NavLink to='#activities' className='links'><b>{state.activity.total}</b><h5>ACTIVITIES</h5></NavLink></div>
                 <div className='col-md-3 col-3'><NavLink to='#favriate' className='links'><b>{state.favoriate.total} </b><h5>FAVRIATE</h5></NavLink></div>
-                <div className='col-md-3 col-3'><NavLink to='#reviews' className='links'><b>26 </b><h5>REVIEWS</h5></NavLink></div>
-                <div className='col-md-3 col-3'><NavLink to='#points' className='links'><b>126 </b><h5>POINTS</h5></NavLink></div>
+                <div className='col-md-3 col-3'><NavLink to='#reviews' className='links'><b>{state.reviews.total} </b><h5>REVIEWS</h5></NavLink></div>
+                <div className='col-md-3 col-3'><NavLink to='#points' className='links'><b>{state.points.total}</b><h5>POINTS</h5></NavLink></div>
             </div>
             <div className='row post_container'>
-                {tabindex == 'activities' ? <Activity data={state.activity.item} status={1}/> : ""}
+                {tabindex == 'activities' ? <Activity data={state.activity.item}/> : ""}
                 {tabindex == 'favriate' ? <Favriate data={state.favoriate.item}/> : ""}
-                {tabindex == 'reviews' ? "" : ""}
-                {tabindex == 'points' ? "" : ""}
+                {tabindex == 'reviews' ? <Review data={state.reviews.item}/> : ""}
+                {tabindex == 'points' ?<Point data={state.points.item}/> : ""}
 
 
 
--- src/reducer/profileTabReducer.js
+++ src/reducer/profileTabReducer.js
@@ -1,22 +1,42 @@
-const intialState={
-    activity:{total:0,item:[]},
-    favoriate:{total:0,item:[]},
-    reviews:[],
-    points:[]
+const intialState = {
+    activity: { total: 0, item: [] },
+    favoriate: { total: 0, item: [] },
+    reviews:{total:0,item:[]},
+    points: {total:0,item:{}}
 }
 
-const profileTabReducer=(state=intialState,action)=>{
-    switch (action.type){
+const profileTabReducer = (state = intialState, action) => {
+    switch (action.type) {
         case "FATCH_USER_ACTIVITY":
-            return {...state,activity:{
-                       total:action.payload.length,
-                       item:action.payload}}
-        break
+            return {
+                ...state, activity: {
+                    total: action.payload.length,
+                    item: action.payload
+                }
+            }
+            break
         case "FATCH_USER_FAVORIATES":
-            return {...state,favoriate:{
-                total:action.payload.length,
-                item:action.payload}}
-        break
+            return {
+                ...state, favoriate: {
+                    total: action.payload.length,
+                    item: action.payload
+                }
+            }
+            break
+        case "FATCH_USER_FEEDBACK":
+            return {
+                ...state, reviews: {
+                    total: action.payload.length,
+                    item: action.payload
+                }
+            } 
+            break
+        case "FATCH_USER_POINT":
+            return {...state, points:{
+                     total:action.payload.total_point,
+                     item: action.payload
+            }
+            }
         default: return state
     }
 }
