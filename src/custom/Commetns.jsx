e10100f23cf249455e30531cf294a49a0f04dbf9
Saurabh <saurabh.bhatt.saubhagyam@gmail.com>
2022-01-20 16:57:25 +0530

    testing

--- src/custom/Commetns.jsx
+++ src/custom/Commetns.jsx
@@ -6,38 +6,41 @@ import env from '../env'
 const Comments = (props) => {
     let { comment } = useSelector(state => state.postlikesListreducer)
     return (<>
-     <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
-  <div className="modal fade" id="myModal" role="dialog">
-    <div className="modal-dialog">
-    
-      
-      <div className="modal-content">
-        <div className="modal-header">
-          <button type="button" className="close" data-dismiss="modal">&times;</button>
-          <h4 className="modal-title">Modal Header</h4>
+        <div className="form-popup border" style={{zIndex:"99",position:'absolute'}} >
+         {/* <div className='col-sm-4'></div> */}
+            <div className='col-sm-5 text-center   bg-white'>
+                <span style={{
+                    float: 'right',
+                    zIndex: 99,
+                    color: "black",
+                    cursor: "pointer"
+                }}
+                    onClick={() => { props.close(false) }}>&times;</span>
+                <div className="mt-3 col-12 p-2">
+                    <div className="coment-bottom  p-2 px-4">
+                        <h5>Comments</h5>
+                        <div className="d-flex flex-row add-comment-section mt-3 mb-3 col-12">
+                            <img className="img-fluid img-responsive rounded-circle mr-3" src="https://i.imgur.com/qdiP4DB.jpg" width="38" />&nbsp;<input type="text" className="form-control mr-2" placeholder="Add comment" />&nbsp;<button className="btn btn-primary" type="button">Post</button></div>
+                        <hr />
+                        {_.isEmpty(comment)?"List Empty":comment.map((each) => {
+                            return <>
+                                <div className="commented-section">
+                                    <div className="d-flex flex-row add-comment-section mt-3 mb-3 col-12">
+                                        <img className="img-fluid img-responsive rounded-circle mr-3" src={`${env.URL}/dipicious/${each.user_profile_pic}`} width="38" />&nbsp;<b>{each.name==''?"unknown":each.name}</b>&nbsp;&nbsp;{each.time} ago</div>
+
+                                    <p className='text-left ml-5'><span>{each.comment}</span></p>
+
+                                </div>
+
+                            </>
+                        })}
+
+                    </div>
+                </div>
+            </div>
+            <div className='col-sm-3'></div>
         </div>
-        <div className="modal-body">
-          <p>Some text in the modal.</p>
-        </div>
-        <div className="modal-footer">
-          <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-        </div>
-      </div>
-      
-    </div>
-  </div>
+
     </>)
 }
 export default Comments
-// {_.isEmpty(comment)?"List Empty":comment.map((each) => {
-//     return <>
-//         <div classNameName="commented-section">
-//             <div classNameName="d-flex flex-row add-comment-section mt-3 mb-3 col-12">
-//                 <img classNameName="img-fluid img-responsive rounded-circle mr-3" src={`${env.URL}/dipicious/${each.user_profile_pic}`} width="38" />&nbsp;<b>{each.name==''?"unknown":each.name}</b>&nbsp;&nbsp;{each.time} ago</div>
-
-//             <p classNameName='text-left ml-5'><span>{each.comment}</span></p>
-
-//         </div>
-
-// </>
-// ))}
